5450448f4cbcbaf838fc31eeb027a2ff7096ef20
RamiroPinol <ramirop@protonmail.com>
2016-12-19 23:02:10 -0300

    added gameTurn() and bugfixes

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
-let Simon = {
+let Simon = function() {
+
   // sequence of colors
   // 1: green, 2: red, 3: yellow, 4: blue
-  arrayGame : [],
+  this.arrayGame = []
 
   // Push a random number [1,5) to sequence
-  gameMove : function() {
+  this.gameMove = function() {
     this.arrayGame.push(Math.floor(Math.random() * (5 - 1) + 1))
-  },
+  }
 
   // Player's move
-  playerMove : function(color) {
+  this.playerMove = function(color) {
     this.arrayPlayer.push(color)
-  },
-/*
-  play : function() {
-    this.sequence.map(function(color) {
-      if (color == 1) {
-
-      }
-    })
-  },
-  */
+  }
 
   // "iluminates" simon piece by changing color during time and returning to original
-  lightColor : function(color, time) {
+  this.lightColor = function(color, time) {
     const colors = ["#29a529", "#a52929", "#a5a529", "#5656d8"]
     const item = document.querySelector(`div[data-color="${color}"]`)
 
@@ -36,6 +28,33 @@ let Simon = {
     }
 
     setColor(color)
-    const timeout = window.setTimeout(setColor, time)
+    const timeout = setTimeout(setColor, time)
+  }
+
+  this.playSequence = function(i) {
+    setTimeout( () => {
+      this.lightColor(this.arrayGame[i], 1000)
+      if (++i < this.arrayGame.length) {          // If i > 0, keep going
+        this.playSequence(i);       // Call the loop again, and pass it the current value of i
+      }
+    }, 1500);
+  }
+
+  this.gameTurn = function() {
+    this.gameMove()
+    this.playSequence(0)
   }
 }
+/*
+var simon = new Simon()
+simon.gameMove()
+simon.gameMove()
+simon.gameMove()
+*/
+const options = document.querySelectorAll("#main div")
+
+function test(e) {
+  console.log(e.target.attributes["data-color"].value)
+}
+
+options.forEach(option => option.addEventListener("click", test))
